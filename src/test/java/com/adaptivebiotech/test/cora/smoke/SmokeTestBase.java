95db2c27a6596592bf204ca9c78cd1e27a592b97
jzhou <jzhou@adaptivebiotech.com>
2019-12-27 11:23:35 -0800

    1. Formatted the code 2. Added testlog 3. Replaced matches to matches("D-\\d{6}") 4. Removed AfterMethod

--- src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestBase.java
+++ src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestBase.java
@@ -4,22 +4,19 @@ import static org.testng.Assert.assertEquals;
 import com.adaptivebiotech.test.cora.CoraBaseBrowser;
 import com.adaptivebiotech.ui.cora.order.Diagnostic;
 
-
-
-public class SmokeTestBase extends CoraBaseBrowser{
+public class SmokeTestBase extends CoraBaseBrowser {
 	protected Diagnostic diagnostic;
-	
+
 	protected void verifyPhysicianName(String ExpectedName) {
-		 assertEquals (diagnostic.getProviderName(), ExpectedName);
+		assertEquals(diagnostic.getProviderName(), ExpectedName);
 	}
-	
-	//Verify Diagnostic Order page displays an order number, D-######, in the order header.
-	protected void verifyOrderNumber(String expectedOrderNumber) {
-		
+
+	// Verify Diagnostic Order page displays an order number, D-######, in the order
+	// header.
+	protected void verifyOrderNumber() {
+
 		String orderNumber = diagnostic.getOrderNum();
-		assertEquals(orderNumber.length(),expectedOrderNumber.length());
-		assertEquals(orderNumber.substring(0,1),expectedOrderNumber.substring(0,1));
-		assertEquals(orderNumber.substring(2,orderNumber.length()).matches("[0-9]+"),true);
+		assertEquals(orderNumber.matches("D-\\d{6}"), true);
 	}
 
 }
--- src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestSuite.java
+++ src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestSuite.java
@@ -1,5 +1,7 @@
 package com.adaptivebiotech.test.cora.smoke;
 
+import static com.adaptivebiotech.test.utils.Logging.testLog;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -7,37 +9,34 @@ import com.adaptivebiotech.common.dto.Physician;
 import com.adaptivebiotech.ui.cora.CoraPage;
 import com.adaptivebiotech.ui.cora.order.Diagnostic;
 
-@Test (groups = { "smoke" })
-public class SmokeTestSuite extends SmokeTestBase{
-	 private CoraPage     main;
+@Test(groups = { "smoke" })
+public class SmokeTestSuite extends SmokeTestBase {
+	private CoraPage main;
 
+	@BeforeMethod
+	public void beforeMethod() {
+		main = new CoraPage();
+		testLog("Login Cora");
+	}
 
-	    @BeforeMethod
-	    public void beforeMethod () {
-	        main = new CoraPage ();
-	    }
+	// SR-T1960
+	public void SaveNewDiagnosticOrder() {
+		diagnostic = new Diagnostic();
+		Physician physician = new Physician();
+		physician.firstName = "";
+		physician.lastName = "UVT-Physician";
+		physician.accountName = "";
+		physician.providerFullName = "";
+		physician.allowInternalOrderUpload = true;
 
-	    @AfterMethod
-	    public void afterMethod () {
-	       
-	    }
-	    
-	    // SR-T1960
-	    public void SaveNewDiagnosticOrder () {
-	        diagnostic = new Diagnostic();
-	        Physician physician = new Physician ();
-	        physician.firstName = "";
-	        physician.lastName = "UVT-Physician";
-	        physician.accountName = "";
-	        physician.providerFullName = "";
-	        physician.allowInternalOrderUpload = true;
-	        
-	    	main.clickNewDiagnosticOrder();
-	        diagnostic.selectPhysician(physician);
-	        verifyPhysicianName("Matt UVT-Physician");
-	        diagnostic.clickSave();
-	        verifyOrderNumber("D-######");
-	    }
-
+		main.clickNewDiagnosticOrder();
+		testLog("Create new diagnostic order");
+		diagnostic.selectPhysician(physician);
+		testLog("Search and select physician");
+		verifyPhysicianName("Matt UVT-Physician");
+		diagnostic.clickSave();
+		testLog("Save the diagnostic order");
+		verifyOrderNumber();
+	}
 
 }
