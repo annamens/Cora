79a5b8c91047831c9f6b8c164d2a1cbfafc87be4
jzhou <jzhou@adaptivebiotech.com>
2019-12-27 16:40:40 -0800

    Used right formatter to format the code

--- src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestBase.java
+++ src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestBase.java
@@ -5,18 +5,18 @@ import com.adaptivebiotech.test.cora.CoraBaseBrowser;
 import com.adaptivebiotech.ui.cora.order.Diagnostic;
 
 public class SmokeTestBase extends CoraBaseBrowser {
-	protected Diagnostic diagnostic;
+    protected Diagnostic diagnostic;
 
-	protected void verifyPhysicianName(String ExpectedName) {
-		assertEquals(diagnostic.getProviderName(), ExpectedName);
-	}
+    protected void verifyPhysicianName (String ExpectedName) {
+        assertEquals (diagnostic.getProviderName (), ExpectedName);
+    }
 
-	// Verify Diagnostic Order page displays an order number, D-######, in the order
-	// header.
-	protected void verifyOrderNumber() {
+    // Verify Diagnostic Order page displays an order number, D-######, in the order
+    // header.
+    protected void verifyOrderNumber () {
 
-		String orderNumber = diagnostic.getOrderNum();
-		assertEquals(orderNumber.matches("D-\\d{6}"), true);
-	}
+        String orderNumber = diagnostic.getOrderNum ();
+        assertEquals (orderNumber.matches ("D-\\d{6}"), true);
+    }
 
 }
--- src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestSuite.java
+++ src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestSuite.java
@@ -1,7 +1,6 @@
 package com.adaptivebiotech.test.cora.smoke;
 
 import static com.adaptivebiotech.test.utils.Logging.testLog;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,34 +8,34 @@ import com.adaptivebiotech.common.dto.Physician;
 import com.adaptivebiotech.ui.cora.CoraPage;
 import com.adaptivebiotech.ui.cora.order.Diagnostic;
 
-@Test(groups = { "smoke" })
+@Test (groups = { "smoke" })
 public class SmokeTestSuite extends SmokeTestBase {
-	private CoraPage main;
+    private CoraPage main;
 
-	@BeforeMethod
-	public void beforeMethod() {
-		main = new CoraPage();
-		testLog("Login Cora");
-	}
+    @BeforeMethod
+    public void beforeMethod () {
+        main = new CoraPage ();
+        testLog ("Login Cora");
+    }
 
-	// SR-T1960
-	public void SaveNewDiagnosticOrder() {
-		diagnostic = new Diagnostic();
-		Physician physician = new Physician();
-		physician.firstName = "";
-		physician.lastName = "UVT-Physician";
-		physician.accountName = "";
-		physician.providerFullName = "";
-		physician.allowInternalOrderUpload = true;
+    // SR-T1960
+    public void SaveNewDiagnosticOrder () {
+        diagnostic = new Diagnostic ();
+        Physician physician = new Physician ();
+        physician.firstName = "";
+        physician.lastName = "UVT-Physician";
+        physician.accountName = "";
+        physician.providerFullName = "";
+        physician.allowInternalOrderUpload = true;
 
-		main.clickNewDiagnosticOrder();
-		testLog("Create new diagnostic order");
-		diagnostic.selectPhysician(physician);
-		testLog("Search and select physician");
-		verifyPhysicianName("Matt UVT-Physician");
-		diagnostic.clickSave();
-		testLog("Save the diagnostic order");
-		verifyOrderNumber();
-	}
+        main.clickNewDiagnosticOrder ();
+        testLog ("Create new diagnostic order");
+        diagnostic.selectPhysician (physician);
+        testLog ("Search and select physician");
+        verifyPhysicianName ("Matt UVT-Physician");
+        diagnostic.clickSave ();
+        testLog ("Save the diagnostic order");
+        verifyOrderNumber ();
+    }
 
 }
