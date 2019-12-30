54ac2638e37ad01bdc69afc85feda84862f87c91
jzhou <jzhou@adaptivebiotech.com>
2019-12-30 11:23:55 -0800

    Removed unused pkg org.testng.annotations.AfterMethod

--- src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestSuite.java
+++ src/test/java/com/adaptivebiotech/test/cora/smoke/SmokeTestSuite.java
@@ -1,7 +1,6 @@
 package com.adaptivebiotech.test.cora.smoke;
 
 import static com.adaptivebiotech.test.utils.Logging.testLog;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import com.adaptivebiotech.common.dto.Physician;
