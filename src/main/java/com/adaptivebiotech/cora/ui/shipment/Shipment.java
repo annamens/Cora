df0e912c967ad37385b776093fb63b5c648fe8ef
Harry Soehalim <hsoehalim@adaptivebiotech.com>
2020-08-16 01:43:20 -0700

    update

--- src/main/java/com/adaptivebiotech/cora/ui/shipment/Shipment.java
+++ src/main/java/com/adaptivebiotech/cora/ui/shipment/Shipment.java
@@ -48,7 +48,6 @@ public class Shipment extends CoraPage {
     public void clickSave () {
         assertTrue (click ("[data-ng-click*='shipment-save']"));
         pageLoading ();
-        closeNotification ("Shipment saved");
     }
 
     public void enterShippingCondition (ShippingCondition condition) {
