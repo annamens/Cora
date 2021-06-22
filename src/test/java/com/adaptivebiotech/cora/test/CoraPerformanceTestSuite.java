8ae7d225596e83b84fbd64e4c9a459235ca08438
otereshchuk <otereshchuk@adaptivebiotech.com>
2021-06-22 11:53:31 -0700

    performance test suite updated updated test-commons version newBatchShipment temporary removed to investigate more options for capturing performance metrics obtainPerformanceMetrics method will be updated and moved to test-commons with use of rest api

--- src/test/java/com/adaptivebiotech/cora/test/CoraPerformanceTestSuite.java
+++ src/test/java/com/adaptivebiotech/cora/test/CoraPerformanceTestSuite.java
@@ -1,24 +1,25 @@
 package com.adaptivebiotech.cora.test;
 
 import static com.adaptivebiotech.cora.utils.TestHelper.freezerAB018078;
-import static com.adaptivebiotech.test.utils.PageHelper.ShippingCondition.Ambient;
-import static org.testng.Assert.assertTrue;
-import java.util.ArrayList;
-import java.util.List;
+import static com.adaptivebiotech.test.BaseEnvironment.coraTestUrl;
+import static com.adaptivebiotech.test.utils.Logging.testLog;
+import static com.seleniumfy.test.utils.Environment.sauceOptions;
+import java.util.HashMap;
+import java.util.Map;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import com.adaptivebiotech.cora.ui.CoraPage;
 import com.adaptivebiotech.cora.ui.Login;
-import com.adaptivebiotech.cora.ui.container.AddContainer;
 import com.adaptivebiotech.cora.ui.container.ContainerList;
 import com.adaptivebiotech.cora.ui.mira.MirasList;
-import com.adaptivebiotech.cora.ui.order.Diagnostic;
 import com.adaptivebiotech.cora.ui.order.OrderTestsList;
 import com.adaptivebiotech.cora.ui.order.OrdersList;
 import com.adaptivebiotech.cora.ui.patient.PatientsList;
-import com.adaptivebiotech.cora.ui.shipment.Shipment;
 import com.adaptivebiotech.cora.ui.shipment.ShipmentList;
 import com.adaptivebiotech.cora.ui.task.TaskList;
+import com.adaptivebiotech.common.dto.PerformanceMetrics;
 
 @Test (groups = "performance")
 public class CoraPerformanceTestSuite extends CoraBaseBrowser {
@@ -32,9 +33,11 @@ public class CoraPerformanceTestSuite extends CoraBaseBrowser {
     private TaskList       tList;
     private MirasList      mList;
     private PatientsList   pList;
-    private AddContainer   cAdd;
-    private Diagnostic     diagnostic;
-    private Shipment       shipment;
+
+    static {
+        sauceOptions.put ("extendedDebugging", "true");
+        sauceOptions.put ("capturePerformance", "true");
+    }
 
     @BeforeMethod (alwaysRun = true)
     public void beforeMethod () {
@@ -47,71 +50,84 @@ public class CoraPerformanceTestSuite extends CoraBaseBrowser {
         tList = new TaskList ();
         mList = new MirasList ();
         pList = new PatientsList ();
-        cAdd = new AddContainer ();
-        diagnostic = new Diagnostic ();
-        shipment = new Shipment ();
         login.doLogin ();
+    }
+
+    @AfterSuite
+    public void cleanUp () {
+        sauceOptions.remove ("extendedDebugging");
+        sauceOptions.remove ("capturePerformance");
     }
 
     public void loadListOfOrders () {
         oList.isCorrectTopNavRow2 ();
+        navigateTo(coraTestUrl + "/cora/orders");
         oList.isCorrectPage ();
+        PerformanceMetrics listOfOrdersMetrics = obtainPerformanceMetrics();
     }
 
     public void loadListOfOrderTests () {
         oList.isCorrectPage ();
-        cora.clickOrderTests ();
+        //cora.clickOrderTests ();
+        navigateTo(coraTestUrl + "/cora/ordertests");
         otList.isCorrectPage ();
+        PerformanceMetrics listOfOrderTestsMetrics = obtainPerformanceMetrics();
     }
 
     public void loadListOfShipments () {
         oList.isCorrectPage ();
-        cora.clickShipments ();
+        //cora.clickShipments ();
+        navigateTo(coraTestUrl + "/cora/shipments");
         shList.isCorrectPage ();
+        PerformanceMetrics listOfShipmentsMetrics = obtainPerformanceMetrics();
     }
 
     public void loadListOfContainers () {
         oList.isCorrectPage ();
-        cora.clickContainers ();
+        //cora.clickContainers ();
+        navigateTo(coraTestUrl + "/cora/containers");
         cList.isCorrectPage ();
+        PerformanceMetrics listOfContainersMetrics = obtainPerformanceMetrics();
     }
 
     public void loadListOfTasks () {
         oList.isCorrectPage ();
-        cora.clickTasks ();
+        //cora.clickTasks ();
+        navigateTo(coraTestUrl + "/cora/tasks");
         tList.isCorrectPage ();
+        PerformanceMetrics listOfTasksMetrics = obtainPerformanceMetrics();
     }
 
     public void loadListOfMiras () {
         oList.isCorrectPage ();
-        cora.clickMiras ();
+        //cora.clickMiras ();
+        navigateTo(coraTestUrl + "/cora/miras/list");
         mList.isCorrectPage ();
+        PerformanceMetrics listOfMirasMetrics = obtainPerformanceMetrics();
     }
 
     public void loadListOfPatients () {
         oList.isCorrectPage ();
-        cora.clickPatients ();
+        //cora.clickPatients ();
+        navigateTo(coraTestUrl + "/cora/patients/list");
         pList.isCorrectPage ();
+        PerformanceMetrics listOfPatientsMetrics = obtainPerformanceMetrics();
     }
 
     public void filterContainers () {
         oList.isCorrectPage ();
         cora.searchContainer (freezerAB018078 ());
         cList.isCorrectPage ();
+        PerformanceMetrics filterContainersMetrics = obtainPerformanceMetrics();
     }
 
-    public void newBatchShipment () {
-        oList.isCorrectPage ();
-        diagnostic.selectNewBatchShipment ();
-        shipment.isBatchOrGeneral ();
-        shipment.enterShippingCondition (Ambient);
-        shipment.clickSave ();
-        String sNum = shipment.getHeaderShipmentNum ();
-        assertTrue (sNum.matches ("Batch Shipment # SH-\\d{5,}"));
-        cora.clickShipments ();
-        shList.isCorrectPage ();
-        List <String> shNames = new ArrayList <> ();
-        shList.getAllShipments ().stream ().forEach (x -> shNames.add (x.shipmentNumber));
-        assertTrue (shNames.contains (sNum.substring (sNum.indexOf ("SH-"))));
+    private PerformanceMetrics obtainPerformanceMetrics () {
+        testLog ("Current page: " + getDriver ().getCurrentUrl ());
+        HashMap<String, Object> metrics = new HashMap <> ();
+        metrics.put ("type", "sauce:performance");
+        Map<?, ?> perfMetrics = (Map <?, ?>) ((JavascriptExecutor) getDriver ()).executeScript ("sauce:log",
+                metrics);
+        return PerformanceMetrics.buildPerformanceMetrics (perfMetrics);
     }
+
 }
