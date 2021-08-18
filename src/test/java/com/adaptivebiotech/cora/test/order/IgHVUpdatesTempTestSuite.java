7293adcd7df5af12f142e4d841901cfb71c2a7fb
jpatel <jpatel@adaptivebiotech.com>
2021-08-17 17:19:02 -0700

    ignore commit, test file response, attempt 2

--- src/test/java/com/adaptivebiotech/cora/test/order/IgHVUpdatesTempTestSuite.java
+++ src/test/java/com/adaptivebiotech/cora/test/order/IgHVUpdatesTempTestSuite.java
@@ -9,6 +9,9 @@ import java.util.Map;
 import org.apache.http.message.BasicHeader;
 import org.testng.annotations.Test;
 import com.adaptivebiotech.cora.test.CoraBaseBrowser;
+import com.adaptivebiotech.cora.ui.Login;
+import com.adaptivebiotech.cora.ui.order.Diagnostic;
+import com.adaptivebiotech.cora.ui.workflow.History;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.seleniumfy.test.utils.HttpClientHelper;
 
@@ -16,30 +19,43 @@ public class IgHVUpdatesTempTestSuite extends CoraBaseBrowser {
 
     @Test
     public void verifyIgHVStageAndReportFeatureOrder1CLIAFeatureFlagOn () {
-        releaseReport ("https://cora-test.dna.corp.adaptivebiotech.com/cora/debug/file/1940a8b4-6bcb-4d6d-9285-83241fdae3a5/Workflow/reportData.json",
+//        String tempString = "https://cora-test.dna.corp.adaptivebiotech.com/cora/debug/file/1940a8b4-6bcb-4d6d-9285-83241fdae3a5/Workflow/reportData.json";
+        String tempString = "118422-01BC";
+        releaseReport (tempString,
                        true);
     }
 
     @Test
     public void verifyIgHVStageAndReportFeatureOrder2CLIAFeatureFlagOn () {
-        releaseReport ("https://cora-test.dna.corp.adaptivebiotech.com/cora/debug/file/8076f7c0-a4b8-481e-9498-fec9c300485c/Workflow/reportData.json",
+//        String tempString = "https://cora-test.dna.corp.adaptivebiotech.com/cora/debug/file/8076f7c0-a4b8-481e-9498-fec9c300485c/Workflow/reportData.json";
+        String tempString = "118421-01BC";
+        releaseReport (tempString,
                        false);
     }
 
     @Test
     public void verifyIgHVStageAndReportFeatureOrder3IVDFeatureFlagOn () {
-        releaseReport ("https://cora-test.dna.corp.adaptivebiotech.com/cora/debug/file/15e5612a-863f-425d-ba2e-687622041487/Workflow/reportData.json",
+//        String tempString = "https://cora-test.dna.corp.adaptivebiotech.com/cora/debug/file/15e5612a-863f-425d-ba2e-687622041487/Workflow/reportData.json";
+        String tempString = "118420-01MC";
+        releaseReport (tempString,
                        true);
     }
 
     @Test
     public void verifyIgHVStageAndReportFeatureOrder4IVDFeatureFlagOn () {
-        releaseReport ("https://cora-test.dna.corp.adaptivebiotech.com/cora/debug/file/963cc86a-f3ad-4866-92a1-48d404e214e3/Workflow/reportData.json",
+//        String tempString = "https://cora-test.dna.corp.adaptivebiotech.com/cora/debug/file/963cc86a-f3ad-4866-92a1-48d404e214e3/Workflow/reportData.json";
+        String tempString = "118417-01BC";
+        releaseReport (tempString,
                        true);
     }
 
-    private void releaseReport (String fileUrl,
+    private void releaseReport (String sampleName,
                                 boolean expectedShmReportKey) {
+        new Login ().doLogin ();
+        History history = new History ();
+        
+        history.gotoOrderDebug (sampleName);
+        String fileUrl = history.getFileUrl ("reportData.json");
         // get file using get request
         doCoraLogin ();
         Map <String, Object> reportData = null;
