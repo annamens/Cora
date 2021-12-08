da7c3ba2a4bd780bc0ac5a730e9734c4f88e23c5
Harry Soehalim <hsoehalim@adaptivebiotech.com>
2021-12-08 09:53:07 -0800

    all the icon click assume we only have 1 order per patient, fixing this.

--- src/main/java/com/adaptivebiotech/cora/ui/patient/PatientOrderHistory.java
+++ src/main/java/com/adaptivebiotech/cora/ui/patient/PatientOrderHistory.java
@@ -1,13 +1,16 @@
 package com.adaptivebiotech.cora.ui.patient;
 
-import static org.testng.Assert.assertEquals;
+import static java.lang.String.format;
 import static org.testng.Assert.assertTrue;
+import com.adaptivebiotech.cora.dto.Orders.Order;
 
 /**
  * @author jpatel
  *
  */
 public class PatientOrderHistory extends PatientHeader {
+
+    private final String icon = "//tr[td='%s']/td//*[contains (@src, '%s')]";
 
     public PatientOrderHistory () {
         staticNavBarHeight = 200;
@@ -19,32 +22,25 @@ public class PatientOrderHistory extends PatientHeader {
     }
 
     public void closeReportPreview () {
-        String css = ".modal-header button.close";
-        assertTrue (click (css));
-        waitForAjaxCalls ();
+        assertTrue (click (".modal-header button.close"));
+        moduleLoading ();
     }
 
-    public void clickReportPreviewLink () {
-        String css = "img[src='/assets/images/ReportPDF.png']";
-        assertTrue (click (css));
-        String headerText = waitForElementVisible (".modal-header").getText ();
-        assertEquals (headerText, "Preview");
+    public void clickReportPreviewLink (Order order) {
+        assertTrue (click (format (icon, "ReportPDF.png", order.order_number)));
+        assertTrue (isTextInElement (popupTitle, "Preview"));
     }
 
-    public void clickReportNotesIcon () {
-        String css = "img[src='/assets/images/ReportNotes.png']";
-        assertTrue (click (css));
-        waitForAjaxCalls ();
+    public void clickReportNotesIcon (Order order) {
+        assertTrue (click (format (icon, "ReportNotes.png", order.order_number)));
+    }
+
+    public void clickAdditionalCommentsIcon (Order order) {
+        assertTrue (click (format (icon, "ReportPDFAdditionalComments.png", order.order_number)));
     }
 
     public String getTooltipText () {
         String css = ".selectable-tooltip-content";
         return getText (css);
-    }
-
-    public void clickAdditionalCommentsIcon () {
-        String additionalCommentsButton = "img[src='/assets/images/ReportPDFAdditionalComments.png']";
-        assertTrue (click (additionalCommentsButton));
-        waitForAjaxCalls ();
     }
 }
