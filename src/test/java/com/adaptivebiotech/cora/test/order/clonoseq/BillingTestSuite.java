1b826f32edab8015d7fb12c93d45e27a26bfe460
otereshchuk <otereshchuk@adaptivebiotech.com>
2022-04-08 15:12:41 -0700

    Reason test added for ClonoSEQ

--- src/test/java/com/adaptivebiotech/cora/test/order/clonoseq/BillingTestSuite.java
+++ src/test/java/com/adaptivebiotech/cora/test/order/clonoseq/BillingTestSuite.java
@@ -1,24 +1,35 @@
 package com.adaptivebiotech.cora.test.order.clonoseq;
 
 import static com.adaptivebiotech.cora.dto.Containers.ContainerType.Tube;
+import static com.adaptivebiotech.cora.dto.Containers.ContainerType.Vacutainer;
 import static com.adaptivebiotech.cora.dto.Orders.Assay.ID_BCell2_CLIA;
+import static com.adaptivebiotech.cora.dto.Orders.ChargeType.Client;
+import static com.adaptivebiotech.cora.dto.Orders.ChargeType.NoCharge;
+import static com.adaptivebiotech.cora.dto.Orders.NoChargeReason.IncompleteDocumentation;
 import static com.adaptivebiotech.cora.dto.Orders.OrderStatus.Active;
+import static com.adaptivebiotech.cora.dto.Physician.PhysicianType.TDetect_all_payments;
 import static com.adaptivebiotech.cora.dto.Physician.PhysicianType.clonoSEQ_client;
 import static com.adaptivebiotech.cora.dto.Physician.PhysicianType.clonoSEQ_insurance;
 import static com.adaptivebiotech.cora.dto.Physician.PhysicianType.clonoSEQ_medicare;
 import static com.adaptivebiotech.cora.dto.Physician.PhysicianType.clonoSEQ_selfpay;
 import static com.adaptivebiotech.cora.dto.Physician.PhysicianType.clonoSEQ_trial;
+import static com.adaptivebiotech.cora.dto.Shipment.ShippingCondition.Ambient;
 import static com.adaptivebiotech.cora.utils.PageHelper.AbnStatus.NotRequired;
 import static com.adaptivebiotech.cora.utils.PageHelper.AbnStatus.RequiredIncludedBillMedicare;
 import static com.adaptivebiotech.cora.utils.TestHelper.bloodSpecimen;
 import static com.adaptivebiotech.cora.utils.TestHelper.newClientPatient;
 import static com.adaptivebiotech.cora.utils.TestHelper.newInsurancePatient;
 import static com.adaptivebiotech.cora.utils.TestHelper.newMedicarePatient;
+import static com.adaptivebiotech.cora.utils.TestHelper.newNoChargePatient;
 import static com.adaptivebiotech.cora.utils.TestHelper.newSelfPayPatient;
 import static com.adaptivebiotech.cora.utils.TestHelper.newTrialProtocolPatient;
 import static com.adaptivebiotech.test.utils.Logging.testLog;
 import static java.lang.String.format;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+import java.util.List;
+import java.util.Map;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import com.adaptivebiotech.cora.dto.Patient;
@@ -28,17 +39,22 @@ import com.adaptivebiotech.cora.ui.Login;
 import com.adaptivebiotech.cora.ui.order.NewOrderClonoSeq;
 import com.adaptivebiotech.cora.ui.order.OrderDetailClonoSeq;
 import com.adaptivebiotech.cora.ui.order.OrdersList;
+import com.adaptivebiotech.cora.ui.shipment.Accession;
+import com.adaptivebiotech.cora.ui.shipment.NewShipment;
 
 @Test (groups = "regression")
 public class BillingTestSuite extends CoraBaseBrowser {
 
-    private final String        log         = "created an order with billing: %s";
-    private final String[]      icdCodes    = { "V95.43" };
-    private Login               login       = new Login ();
-    private OrdersList          ordersList  = new OrdersList ();
-    private NewOrderClonoSeq    diagnostic  = new NewOrderClonoSeq ();
-    private OrderDetailClonoSeq orderDetail = new OrderDetailClonoSeq ();
-    private Specimen            specimen    = bloodSpecimen ();
+    private final String        log                 = "created an order with billing: %s";
+    private final String[]      icdCodes            = { "V95.43" };
+    private final String        noChargeReasonQuery = "select no_charge_reason from cora.order_billing ob join cora.orders o on ob.order_id = o.id where o.order_number =";
+    private Login               login               = new Login ();
+    private OrdersList          ordersList          = new OrdersList ();
+    private NewOrderClonoSeq    diagnostic          = new NewOrderClonoSeq ();
+    private OrderDetailClonoSeq orderDetail         = new OrderDetailClonoSeq ();
+    private Specimen            specimen            = bloodSpecimen ();
+    private NewShipment         shipment            = new NewShipment ();
+    private Accession           accession           = new Accession ();
 
     @BeforeMethod
     public void beforeMethod () {
@@ -147,5 +163,51 @@ public class BillingTestSuite extends CoraBaseBrowser {
                                         Active,
                                         Tube);
         testLog (format (log, patient.billingType.label));
+    }
+
+    /**
+     * @sdlc.requirements SR-7593
+     */
+    @Test (groups = "entlebucher")
+    public void verifyNoChargeReasonIsRequired () {
+        String orderNum = diagnostic.createClonoSeqOrder (coraApi.getPhysician (TDetect_all_payments),
+                                                          newNoChargePatient (),
+                                                          icdCodes,
+                                                          ID_BCell2_CLIA,
+                                                          specimen);
+        shipment.selectNewDiagnosticShipment ();
+        shipment.isDiagnostic ();
+        shipment.enterShippingCondition (Ambient);
+        shipment.enterOrderNumber (orderNum);
+        shipment.selectDiagnosticSpecimenContainerType (Vacutainer);
+        shipment.clickSave ();
+        shipment.clickAccessionTab ();
+        accession.completeAccession ();
+        diagnostic.isCorrectPage ();
+        diagnostic.waitForSpecimenDelivery ();
+        diagnostic.billing.selectBilling (Client);
+        assertFalse (diagnostic.billing.isReasonVisible ());
+        testLog ("Reason drop down is not visible when anything but No Charge is picked as billing option");
+
+        diagnostic.billing.selectBilling (NoCharge);
+        testLog ("No Charge is available as billing option for T Detect");
+
+        assertTrue (diagnostic.billing.isReasonVisible ());
+        testLog ("Reason drop down is visible when No Charge is picked as billing option");
+
+        diagnostic.clickSaveAndActivate ();
+        assertTrue (diagnostic.isErrorTextVisible ("Required!"));
+        testLog ("Reason is required when No Charge is picked as billing option");
+
+        diagnostic.billing.selectReason (IncompleteDocumentation);
+        diagnostic.clickSaveAndActivate ();
+        diagnostic.waitUntilActivated ();
+        testLog ("Order activated");
+
+        List <Map <String, Object>> queryResults = coraDb.executeSelect (noChargeReasonQuery + "'" + orderNum + "'");
+        assertEquals (queryResults.size (), 1);
+        Map <String, Object> queryEmrData = queryResults.get (0);
+        assertEquals (queryEmrData.get ("no_charge_reason").toString (), IncompleteDocumentation.label);
+        testLog ("No charge reason is saved in DB");
     }
 }
