60e1e20625b775f24a7e93961cf3bb38e31305eb
Harry Soehalim <hsoehalim@adaptivebiotech.com>
2022-05-24 16:49:09 -0700

    for cell pellet and cell suspension, we must provide cell count

--- src/main/java/com/adaptivebiotech/cora/ui/order/NewOrderClonoSeq.java
+++ src/main/java/com/adaptivebiotech/cora/ui/order/NewOrderClonoSeq.java
@@ -7,8 +7,10 @@ import static com.adaptivebiotech.cora.dto.Orders.NoChargeReason.NoReportIssued;
 import static com.adaptivebiotech.cora.dto.Orders.OrderStatus.Active;
 import static com.adaptivebiotech.test.utils.DateHelper.formatDt7;
 import static com.adaptivebiotech.test.utils.PageHelper.SpecimenType.CellPellet;
+import static com.adaptivebiotech.test.utils.PageHelper.SpecimenType.CellSuspension;
 import static com.seleniumfy.test.utils.Logging.info;
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.BooleanUtils.toBoolean;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -363,7 +365,7 @@ public class NewOrderClonoSeq extends NewOrder {
             enterSpecimenSource (specimen.sampleSource);
         if (specimen.anticoagulant != null)
             enterAntiCoagulant (specimen.anticoagulant);
-        if (CellPellet.equals (specimen.sampleType))
+        if (asList (CellPellet, CellSuspension).contains (specimen.sampleType))
             enterCellCount (1000000);
 
         enterCollectionDate (specimen.collectionDate.toString ());
