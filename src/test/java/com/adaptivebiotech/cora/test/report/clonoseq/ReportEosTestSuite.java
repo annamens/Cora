faa6ce447c77518fc1c24478ed755ec56327e224
Suresh Babu Garine <sgarine@adaptivebiotech.com>
2022-08-19 10:12:43 -0700

    Added test case IDs

--- src/test/java/com/adaptivebiotech/cora/test/report/clonoseq/ReportEosTestSuite.java
+++ src/test/java/com/adaptivebiotech/cora/test/report/clonoseq/ReportEosTestSuite.java
@@ -59,6 +59,7 @@ public class ReportEosTestSuite extends ReportTestBase {
     }
 
     /**
+     * NOTE: SR-T4276
      * @sdlc.requirements SR-633, SR-1017, SR-1016, SR-1014, SR-1012, SR-1011, SR-1009, SR-1007,
      *                    SR-630, SR-4103, SR-4072, SR-4073, SR-4922, SR-12121 
      */
@@ -178,6 +179,7 @@ public class ReportEosTestSuite extends ReportTestBase {
     }
 
     /**
+     * NOTE: SR-T4276
      * @sdlc.requirements SR-633, SR-1017, SR-1016, SR-1014, SR-1012, SR-1011, SR-1009, SR-1007,
      *                    SR-630, SR-4103, SR-4072, SR-4073, SR-5428, SR-12120
      */
