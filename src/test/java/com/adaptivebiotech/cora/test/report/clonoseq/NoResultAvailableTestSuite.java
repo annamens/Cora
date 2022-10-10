ecfb62916732a08a3ec743951fc5602d40a4bebd
Harry Soehalim <hsoehalim@adaptivebiotech.com>
2022-10-10 11:20:47 -0700

    1) move no result available test from CellFreeDnaTestSuite to NoResultAvailableTestSuite 2) fixing timeout downloading asset from saucelabs

--- src/test/java/com/adaptivebiotech/cora/test/report/clonoseq/NoResultAvailableTestSuite.java
+++ src/test/java/com/adaptivebiotech/cora/test/report/clonoseq/NoResultAvailableTestSuite.java
@@ -7,11 +7,17 @@ import static com.adaptivebiotech.cora.dto.Orders.Assay.ID_BCell2_CLIA;
 import static com.adaptivebiotech.cora.dto.Orders.Assay.ID_BCell2_IVD;
 import static com.adaptivebiotech.cora.dto.Orders.Assay.MRD_BCell2_CLIA;
 import static com.adaptivebiotech.cora.dto.Orders.Assay.MRD_BCell2_IVD;
+import static com.adaptivebiotech.cora.dto.Specimen.Anticoagulant.Streck;
 import static com.adaptivebiotech.cora.utils.PageHelper.QC.Pass;
 import static com.adaptivebiotech.cora.utils.PdfUtil.getPageCount;
 import static com.adaptivebiotech.cora.utils.PdfUtil.getTextFromPDF;
 import static com.adaptivebiotech.cora.utils.TestHelper.scenarioBuilderPatient;
+import static com.adaptivebiotech.cora.utils.TestScenarioBuilder.dateToArrInt;
+import static com.adaptivebiotech.cora.utils.TestScenarioBuilder.stage;
+import static com.adaptivebiotech.test.utils.DateHelper.genLocalDateTime;
 import static com.adaptivebiotech.test.utils.Logging.testLog;
+import static com.adaptivebiotech.test.utils.PageHelper.Compartment.CellFree;
+import static com.adaptivebiotech.test.utils.PageHelper.SpecimenType.Plasma;
 import static com.adaptivebiotech.test.utils.PageHelper.StageName.Clarity;
 import static com.adaptivebiotech.test.utils.PageHelper.StageName.ClonoSEQReport;
 import static com.adaptivebiotech.test.utils.PageHelper.StageStatus.Awaiting;
@@ -28,6 +34,8 @@ import com.adaptivebiotech.cora.dto.Patient;
 import com.adaptivebiotech.cora.test.report.ReportTestBase;
 import com.adaptivebiotech.cora.ui.Login;
 import com.adaptivebiotech.cora.ui.debug.OrcaHistory;
+import com.adaptivebiotech.cora.ui.order.OrderDetailClonoSeq;
+import com.adaptivebiotech.cora.ui.order.OrderStatus;
 import com.adaptivebiotech.cora.ui.order.ReportClonoSeq;
 import com.adaptivebiotech.picasso.dto.ReportRender;
 import com.adaptivebiotech.picasso.dto.verify.ClonoSeq;
@@ -39,10 +47,14 @@ import com.adaptivebiotech.picasso.dto.verify.ClonoSeq;
 @Test (groups = "regression")
 public class NoResultAvailableTestSuite extends ReportTestBase {
 
-    private Login                login       = new Login ();
-    private OrcaHistory          history     = new OrcaHistory ();
-    private ReportClonoSeq       report      = new ReportClonoSeq ();
-    private ThreadLocal <String> downloadDir = new ThreadLocal <> ();
+    private Login                login                = new Login ();
+    private OrderStatus          orderStatus          = new OrderStatus ();
+    private OrderDetailClonoSeq  orderDetail          = new OrderDetailClonoSeq ();
+    private OrcaHistory          history              = new OrcaHistory ();
+    private ReportClonoSeq       report               = new ReportClonoSeq ();
+    private ThreadLocal <String> downloadDir          = new ThreadLocal <> ();
+    private final String         noResultsAvailable   = "No result available";
+    private final String         mrdResultDescription = "This sample failed the quality control criteria despite multiple sequencing attempts, exceeded the sample stability time period, or there was a problem processing the test. Please contact Adaptive Biotechnologies for more information, to provide sample disposition instructions, and/or to discuss whether sending a new sample (if one is available) should be considered.";
 
     @BeforeMethod (alwaysRun = true)
     public void beforeMethod (Method test) {
@@ -58,7 +70,7 @@ public class NoResultAvailableTestSuite extends ReportTestBase {
     public void verify_clia_eos_report () {
         Patient patient = scenarioBuilderPatient ();
         Diagnostic diagnostic = buildCdxOrder (patient,
-                                               null,
+                                               stage (Clarity, Failed),
                                                genCDxTest (ID_BCell2_CLIA, null),
                                                genCDxTest (MRD_BCell2_CLIA, null));
         assertEquals (coraApi.newBcellOrder (diagnostic).patientId, patient.id);
@@ -66,11 +78,12 @@ public class NoResultAvailableTestSuite extends ReportTestBase {
 
         OrderTest orderTest = diagnostic.findOrderTest (ID_BCell2_CLIA);
         login.doLogin ();
-        history.gotoOrderDebug (orderTest.sampleName);
-        history.forceStatusUpdate (Clarity, Failed);
-        history.waitFor (ClonoSEQReport, Awaiting, CLINICAL_QC);
-        history.clickOrderTest ();
-        report.clickReportTab (ID_BCell2_CLIA);
+        orderStatus.gotoOrderStatusPage (orderTest.orderId);
+        orderStatus.isCorrectPage ();
+        orderStatus.waitFor (orderTest.sampleName, ClonoSEQReport, Awaiting, CLINICAL_QC);
+        orderStatus.gotoOrderDetailsPage (orderTest.orderId);
+        orderDetail.isCorrectPage ();
+        orderDetail.clickReportTab (ID_BCell2_CLIA);
         report.releaseReport (ID_BCell2_CLIA, Pass);
 
         String actualPdf = join ("/", downloadDir.get (), orderTest.sampleName + ".pdf");
@@ -91,11 +104,12 @@ public class NoResultAvailableTestSuite extends ReportTestBase {
         testLog ("[CLIA] the EOS ClonoSEQ 2.0 clonality report matched with the baseline");
 
         orderTest = diagnostic.findOrderTest (MRD_BCell2_CLIA);
-        history.gotoOrderDebug (orderTest.sampleName);
-        history.forceStatusUpdate (Clarity, Failed);
-        history.waitFor (ClonoSEQReport, Awaiting, CLINICAL_QC);
-        history.clickOrderTest ();
-        report.clickReportTab (MRD_BCell2_CLIA);
+        orderStatus.gotoOrderStatusPage (orderTest.orderId);
+        orderStatus.isCorrectPage ();
+        orderStatus.waitFor (orderTest.sampleName, ClonoSEQReport, Awaiting, CLINICAL_QC);
+        orderStatus.gotoOrderDetailsPage (orderTest.orderId);
+        orderDetail.isCorrectPage ();
+        orderDetail.clickReportTab (MRD_BCell2_CLIA);
         report.releaseReport (MRD_BCell2_CLIA, Pass);
 
         actualPdf = join ("/", downloadDir.get (), orderTest.sampleName + ".pdf");
@@ -124,7 +138,7 @@ public class NoResultAvailableTestSuite extends ReportTestBase {
     public void verify_ivd_eos_report () {
         Patient patient = scenarioBuilderPatient ();
         Diagnostic diagnostic = buildCdxOrder (patient,
-                                               null,
+                                               stage (Clarity, Failed),
                                                genCDxTest (ID_BCell2_IVD, azTsvPath + "/above-loq.id.tsv.gz"),
                                                genCDxTest (MRD_BCell2_IVD, azTsvPath + "/above-loq.mrd.tsv.gz"));
         assertEquals (coraApi.newBcellOrder (diagnostic).patientId, patient.id);
@@ -132,11 +146,12 @@ public class NoResultAvailableTestSuite extends ReportTestBase {
 
         OrderTest orderTest = diagnostic.findOrderTest (ID_BCell2_IVD);
         login.doLogin ();
-        history.gotoOrderDebug (orderTest.sampleName);
-        history.forceStatusUpdate (Clarity, Failed);
-        history.waitFor (ClonoSEQReport, Awaiting, CLINICAL_QC);
-        history.clickOrderTest ();
-        report.clickReportTab (ID_BCell2_IVD);
+        orderStatus.gotoOrderStatusPage (orderTest.orderId);
+        orderStatus.isCorrectPage ();
+        orderStatus.waitFor (orderTest.sampleName, ClonoSEQReport, Awaiting, CLINICAL_QC);
+        orderStatus.gotoOrderDetailsPage (orderTest.orderId);
+        orderDetail.isCorrectPage ();
+        orderDetail.clickReportTab (ID_BCell2_IVD);
         report.releaseReport (ID_BCell2_IVD, Pass);
 
         String actualPdf = join ("/", downloadDir.get (), orderTest.sampleName + ".pdf");
@@ -157,11 +172,12 @@ public class NoResultAvailableTestSuite extends ReportTestBase {
         testLog ("[IVD] the EOS ClonoSEQ 2.0 clonality report matched with the baseline");
 
         orderTest = diagnostic.findOrderTest (MRD_BCell2_IVD);
-        history.gotoOrderDebug (orderTest.sampleName);
-        history.forceStatusUpdate (Clarity, Failed);
-        history.waitFor (ClonoSEQReport, Awaiting, CLINICAL_QC);
-        history.clickOrderTest ();
-        report.clickReportTab (MRD_BCell2_IVD);
+        orderStatus.gotoOrderStatusPage (orderTest.orderId);
+        orderStatus.isCorrectPage ();
+        orderStatus.waitFor (orderTest.sampleName, ClonoSEQReport, Awaiting, CLINICAL_QC);
+        orderStatus.gotoOrderDetailsPage (orderTest.orderId);
+        orderDetail.isCorrectPage ();
+        orderDetail.clickReportTab (MRD_BCell2_IVD);
         report.releaseReport (MRD_BCell2_IVD, Pass);
 
         actualPdf = join ("/", downloadDir.get (), orderTest.sampleName + ".pdf");
@@ -179,5 +195,98 @@ public class NoResultAvailableTestSuite extends ReportTestBase {
         clonoseq.pageSize = getPageCount (actualPdf);
         verifyReport (clonoseq, getTextFromPDF (actualPdf));
         testLog ("[IVD] the EOS ClonoSEQ 2.0 tracking report matched with the baseline");
+    }
+
+    /**
+     * NOTE: SR-T4204
+     * 
+     * @sdlc.requirements SR-10414:R1
+     */
+    public void cfDnaPlasmaNoResultAvailable () {
+        Patient patient = scenarioBuilderPatient ();
+        Diagnostic diagnostic = buildCdxOrder (patient,
+                                               stage (Clarity, Failed),
+                                               genCDxTest (MRD_BCell2_CLIA, null));
+        diagnostic.specimen.sampleType = Plasma;
+        diagnostic.specimen.properties.Compartment = CellFree;
+        diagnostic.specimen.collectionDate = dateToArrInt (genLocalDateTime (-3));
+        assertEquals (coraApi.newBcellOrder (diagnostic).patientId, patient.id);
+        testLog ("[CLIA] submitted tracking orders");
+
+        OrderTest orderTest = diagnostic.findOrderTest (MRD_BCell2_CLIA);
+        testLog ("Order No: " + orderTest.orderName + ", forced status updated to Clarity -> Failed");
+
+        login.doLogin ();
+        orderStatus.gotoOrderStatusPage (orderTest.orderId);
+        orderStatus.isCorrectPage ();
+        orderStatus.waitFor (orderTest.sampleName, ClonoSEQReport, Awaiting, CLINICAL_QC);
+        orderStatus.gotoOrderDetailsPage (orderTest.orderId);
+        orderDetail.isCorrectPage ();
+        orderDetail.clickReportTab (MRD_BCell2_CLIA);
+        report.releaseReport (MRD_BCell2_CLIA, Pass);
+        testLog ("Order Number: " + orderTest.orderName + ", Released Report, Failure Report Generated");
+
+        String actualPdf = join ("/", downloadDir.get (), orderTest.sampleName + ".pdf");
+        coraApi.get (report.getReportUrl (), actualPdf);
+        history.gotoOrderDebug (orderTest.sampleName);
+
+        coraDebugApi.login ();
+        String actual = join ("/", downloadDir.get (), orderTest.sampleName, reportData);
+        coraDebugApi.get (history.getFileLocation (reportData), actual);
+        testLog ("downloaded " + reportData);
+
+        ClonoSeq clonoseq = basicClonoSeq (parseReportData (actual), patient, diagnostic, orderTest);
+        clonoseq.helper.isCLIA = true;
+        clonoseq.pageSize = getPageCount (actualPdf);
+        String extractedText = getTextFromPDF (actualPdf);
+        verifyReport (clonoseq, extractedText);
+        validatePdfContent (extractedText, noResultsAvailable);
+        validatePdfContent (extractedText, mrdResultDescription);
+    }
+
+    /**
+     * NOTE: SR-T4204
+     * 
+     * @sdlc.requirements SR-10414:R1
+     */
+    public void cfDnaBloodNoResultAvailable () {
+        Patient patient = scenarioBuilderPatient ();
+        Diagnostic diagnostic = buildCdxOrder (patient,
+                                               stage (Clarity, Failed),
+                                               genCDxTest (MRD_BCell2_CLIA, null));
+        diagnostic.specimen.properties.Compartment = CellFree;
+        diagnostic.specimen.properties.Anticoagulant = Streck;
+        assertEquals (coraApi.newBcellOrder (diagnostic).patientId, patient.id);
+        testLog ("[CLIA] submitted tracking orders");
+
+        OrderTest orderTest = diagnostic.findOrderTest (MRD_BCell2_CLIA);
+        testLog ("Order No: " + orderTest.orderName + ", forced status updated to Clarity -> Failed");
+
+        login.doLogin ();
+        orderStatus.gotoOrderStatusPage (orderTest.orderId);
+        orderStatus.isCorrectPage ();
+        orderStatus.waitFor (orderTest.sampleName, ClonoSEQReport, Awaiting, CLINICAL_QC);
+        orderStatus.gotoOrderDetailsPage (orderTest.orderId);
+        orderDetail.isCorrectPage ();
+        orderDetail.clickReportTab (MRD_BCell2_CLIA);
+        report.releaseReport (MRD_BCell2_CLIA, Pass);
+        testLog ("Order Number: " + orderTest.orderName + ", Released Report, Failure Report Generated");
+
+        String actualPdf = join ("/", downloadDir.get (), orderTest.sampleName + ".pdf");
+        coraApi.get (report.getReportUrl (), actualPdf);
+        history.gotoOrderDebug (orderTest.sampleName);
+
+        coraDebugApi.login ();
+        String actual = join ("/", downloadDir.get (), orderTest.sampleName, reportData);
+        coraDebugApi.get (history.getFileLocation (reportData), actual);
+        testLog ("downloaded " + reportData);
+
+        ClonoSeq clonoseq = basicClonoSeq (parseReportData (actual), patient, diagnostic, orderTest);
+        clonoseq.helper.isCLIA = true;
+        clonoseq.pageSize = getPageCount (actualPdf);
+        String extractedText = getTextFromPDF (actualPdf);
+        verifyReport (clonoseq, extractedText);
+        validatePdfContent (extractedText, noResultsAvailable);
+        validatePdfContent (extractedText, mrdResultDescription);
     }
 }
